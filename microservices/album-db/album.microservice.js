19b545b5aff4f8d6101b629b7a42ca7bd0236c91
Cuadri84 <davidcuadrillerosaura@gmail.com>
2023-08-11 12:47:29 +0200

    fixed problem with the find microservice to show all the albums

--- microservices/album-db/album.microservice.js
+++ microservices/album-db/album.microservice.js
@@ -1,30 +1,34 @@
-const mongo = require('mongodb');
-const AlbumModel = require('./album.model');
-const check = require('check-types');
-const httpStatusCodes = require('http-status-codes').StatusCodes;
+const mongo = require("mongodb");
+const AlbumModel = require("./album.model");
+const check = require("check-types");
+const httpStatusCodes = require("http-status-codes").StatusCodes;
 
 /**
  * @class AlbumDbMicroservice
  * @classdesc Album Database Microservice
  */
 class AlbumDbMicroservice {
-/***************************************************************
- ********************* STANDARD METHODS ************************
- ***************************************************************/
+  /***************************************************************
+   ********************* STANDARD METHODS ************************
+   ***************************************************************/
 
-    /**
-     * @summary Create new document
-     * @description Create and insert a new document in a collection.
-     * @param {express.Request} req is the request of the operation
-     * @param {express.Response} res is the response of the operation
-     * @param {express.Next} next is the middleware to continue with code execution
-     * @returns {Array} with created document
-     */
+  /**
+   * @summary Create new document
+   * @description Create and insert a new document in a collection.
+   * @param {express.Request} req is the request of the operation
+   * @param {express.Response} res is the response of the operation
+   * @param {express.Next} next is the middleware to continue with code execution
+   * @returns {Array} with created document
+   */
   create = async (req, res, next) => {
-    if (check.not.assigned(req.body) || check.not.object(req.body) || check.emptyObject(req.body)) {
-        const error = new Error('A non-empty JSON body is mandatory.');
-        next(error);
-        return;
+    if (
+      check.not.assigned(req.body) ||
+      check.not.object(req.body) ||
+      check.emptyObject(req.body)
+    ) {
+      const error = new Error("A non-empty JSON body is mandatory.");
+      next(error);
+      return;
     }
 
     try {
@@ -36,64 +40,65 @@ class AlbumDbMicroservice {
       next(error);
       return;
     }
-  }
+  };
 
-    /**
-     * @summary Find documents from a collection
-     * @description Get documents
-     * @param {express.Request} req is the request of the operation
-     * @param {express.Response} res is the response of the operation
-     * @param {express.Next} next is the middleware to continue with code execution
-     * @returns {Array} with all documents matching the conditions
-     */
+  /**
+   * @summary Find documents from a collection
+   * @description Get documents
+   * @param {express.Request} req is the request of the operation
+   * @param {express.Response} res is the response of the operation
+   * @param {express.Next} next is the middleware to continue with code execution
+   * @returns {Array} with all documents matching the conditions
+   */
   find = async (req, res, next) => {
     try {
-      const findQuery = { _id: { $ne: null }};
+      const findQuery = { _id: { $ne: null } };
 
       const findResult = await AlbumModel.find(findQuery).lean().exec();
 
-      res.status(httpStatusCodes.OK).send(findResult[0]);
+      res.status(httpStatusCodes.OK).send(findResult);
     } catch (error) {
       next(error);
       return;
     }
-  }
+  };
 
-    /**
-     * @summary Update a document
-     * @description Update a document by id
-     * @param {express.Request} req is the request of the operation
-     * @param {express.Response} res is the response of the operation
-     * @param {express.Next} next is the middleware to continue with code execution
-     * @returns {Object} Empty object if the operation went well
-     */
-  updateById = async (req, res, next) => { }
+  /**
+   * @summary Update a document
+   * @description Update a document by id
+   * @param {express.Request} req is the request of the operation
+   * @param {express.Response} res is the response of the operation
+   * @param {express.Next} next is the middleware to continue with code execution
+   * @returns {Object} Empty object if the operation went well
+   */
+  updateById = async (req, res, next) => {};
 
-    /**
-     * @summary Delete a document
-     * @description Delete a document by id
-     * @param {express.Request} req is the request of the operation
-     * @param {express.Response} res is the response of the operation
-     * @param {express.Next} next is the middleware to continue with code execution
-     * @returns {Object} Empty object if the operation went well
-     */
+  /**
+   * @summary Delete a document
+   * @description Delete a document by id
+   * @param {express.Request} req is the request of the operation
+   * @param {express.Response} res is the response of the operation
+   * @param {express.Next} next is the middleware to continue with code execution
+   * @returns {Object} Empty object if the operation went well
+   */
   deleteById = async (req, res, next) => {
-
     let documentId = req.params.id;
     let deleteQuery = { _id: documentId };
 
-    AlbumModel.deleteOne(deleteQuery).then(deleteResult => {
-      if(check.not.assigned(deleteResult)){
-        let error = new Error('Document to delete was not found.');
-        next(error);
-        return;
-      }
+    AlbumModel.deleteOne(deleteQuery)
+      .then((deleteResult) => {
+        if (check.not.assigned(deleteResult)) {
+          let error = new Error("Document to delete was not found.");
+          next(error);
+          return;
+        }
 
-      res.status(httpStatusCodes.OK).send({});
-    }).catch(error => {
-      next(error);
-    });
-  }
+        res.status(httpStatusCodes.OK).send({});
+      })
+      .catch((error) => {
+        next(error);
+      });
+  };
 }
 
 module.exports = AlbumDbMicroservice;
